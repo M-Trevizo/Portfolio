1c6136d14e868bb0b52d192ddb0a98636a5ced6a
M-Trevizo <michael.trevizo93@gmail.com>
2023-03-05 19:35:12 -0800

    Get dropdown script working

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 //Dropdown menu logic
 const dropDown = e => {
-    
+
+    const description = e.target.nextElementSibling;
+    if(description.hasAttribute('hidden')) {
+        description.removeAttribute('hidden');
+    }
+    else {
+        description.setAttribute('hidden', "");
+    }
+
 }
+
+const projectOne = document.getElementById('project-one');
+const porjectTwo = document.getElementById('project-two');
+projectOne.onclick = dropDown;
+porjectTwo.onclick = dropDown;
